ee88e9750158b433d2f948bf309ccf0ca4f97645
Yosuke Yanai <y1y1ytti@gmail.com>
2016-11-18 09:05:32 +0000

    add posting microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.order(created_at: :desc) 
   end
   
   def new
